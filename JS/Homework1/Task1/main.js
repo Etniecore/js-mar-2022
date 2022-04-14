2e295ce799b015ae9acc6e2a7ec375348c347814
Etniecore <zavadovskyi.b@gmail.com>
2022-04-14 18:58:27 +0300

    Homework 14.04.2022

---
+++ JS/Homework1/Task1/main.js
@@ -0,0 +1,105 @@
+let greeting = 'hello';
+console.log(greeting);
+alert(greeting);
+document.write(greeting);
+
+let school = 'owu';
+console.log(school);
+alert(school);
+document.write(school);
+
+let web = 'com';
+console.log(web);
+alert(web);
+document.write(web);
+
+let contry = 'ua';
+console.log(contry);
+alert(contry);
+document.write(contry);
+
+let a = 1;
+console.log(a);
+alert(a);
+document.write(a);
+
+let b = 10;
+console.log(b);
+alert(b);
+document.write(b);
+
+let c = -999;
+console.log(c);
+alert(c);
+document.write(c);
+
+let d = 123;
+console.log(d);
+alert(d);
+document.write(d);
+
+let p = 3.14;
+console.log(p);
+alert(p);
+document.write(p);
+
+let e = 2.7;
+console.log(e);
+alert(e);
+document.write(e);
+
+let g = 16
+console.log(g);
+alert(g);
+document.write(g);
+
+let f = false;
+console.log(f);
+alert(f);
+document.write(f);
+
+let t = true;
+console.log(t);
+alert(t);
+document.write(t);
+
+let bookOne = {
+    name: 'Nineteen Eighty-Four',
+    pages: 328,
+    genre: 'Dystopian'
+}
+
+let bookTwo = {
+    name: 'The Trial',
+    pages: 305,
+    authors: ['Franz Kafka', 'Max Brod', 'Edwin Muir']
+}
+let firstName = 'Bohdan'
+let middleName = 'Miroslawovich'
+let lastName = 'Zavadovskyi'
+let person = lastName + firstName + middleName
+
+
+let name = prompt( 'Імя', 'Bohdan');
+let midName = prompt('По батькові', 'Miroslawovich');
+let age = prompt( 'Вік',25);
+console.log(`${name} ${midName} ${age}`);
+
+a = 100;
+b = '100';
+c = true;
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+
+let books = [
+    { name: 'The Trial', pages: 305, genre: 'novel', author: 'Franz Kafka'},
+    { name: 'Nineteen Eighty-Four', pages: 325, genre: 'dystopian', author: 'George Orwell'},
+    { name: 'Nothern Lights', page:399, genre: 'novel', author: 'Philip Pullman'}
+]
+console.log(books[0])
+console.log(books[1])
+console.log(books[2])
+
+
